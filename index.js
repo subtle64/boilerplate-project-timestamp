c3f9f0a67e70dd7c3148c52e3cc408c60e4028a2
Jason Adriel <143072575+subtle64@users.noreply.github.com>
2024-09-20 16:56:22 +0000

    Tests pass.

--- index.js
+++ index.js
@@ -20,8 +20,15 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function (req, res) {
+  let date = req.params.date ?? Date.now()
+  date = isNaN(date) ? date : parseInt(date)
+  date = new Date(date)
+
+  if (date.toString() === "Invalid Date") 
+    return res.json({ error : "Invalid Date" })
+
+  res.json({"unix": date.getTime(), "utc": date.toUTCString()});
 });
 
 
